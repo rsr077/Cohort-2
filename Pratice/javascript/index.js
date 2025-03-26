f69e1e9ce41d9d3c660548cf7f1de7d675f514c6
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-03-26 10:01:07 +0530

    change

---
+++ Pratice/javascript/index.js
@@ -0,0 +1,13 @@
+function float(value){
+
+  let requestCount = parseFloat(value);
+  console.log(requestCount)
+}
+
+// Example Usage for parseFloat
+
+float("3.14");
+float("42");
+float("42px");
+
+ 
