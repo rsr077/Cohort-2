077f41c18358e7cf97c8c191484c8d1d789644e1
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-03-28 18:49:49 +0530

    fronted

---
+++ New-Year-App/fronted/src/components/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function CreateTodo() {
+  return <div>
+     <input type="text" placeholder="title"></input><br/>
+     <input type="text"placeholder="description"></input>
+     <button>Add a todo</button>
+  </div>
+}
