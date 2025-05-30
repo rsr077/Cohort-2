9b3c229a22ec5b7843421182f7a162944efdf933
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-05-30 06:17:14 +0530

    new

--- Todo-app/frontend/src/components/CreateTodo.jsx
+++ Todo-app/frontend/src/components/CreateTodo.jsx
@@ -1,50 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
 
- export function CreateTodo() {
+export function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-   const [title,setTitle] = useState("");
-   const [description, setDescription] = useState("")
-
-     return <div>
-      <input id="title" style={{
-        padding:10,
-        margin: 10
-      }} type = "text" placeholder="title"  onChange={function(e) {
-         const value = e.target.value;
-         setTitle(e.target.value);
-         document.getElementById("titile").innerHTML
-      }} />
-      <br/>
-      <input  style={{
-        padding:10,
-        margin: 10
-      }} type="text" placeholder="descritpion" onChange={function(e) {
-         const value = e.target.value;
-         setDescription(e.target.value);
-      }}/>   <br/>
-      <button   style={{
-        padding:10,
-        margin: 10
-      }} onClick={()=> {
-  
-          // axiso
-
-         fetch("http://localhost:3000/todos", {
-          method: "POST",
-          body: JSON.stringify( {
-            title: title,
-            descritption: description 
-          }),
-          headers: {
-             "contentType": "application/json"
-          }
-         })
-         .then(async function(res) {
-           const json = await res.json();
-            alert("Todo is complted")
-         })
-      }}>Add to todo</button>
-     </div>
-  }
-
-  
+  return (
+    <div>
+      <input
+        id="title"
+        style={{ padding: 10, margin: 10 }}
+        type="text"
+        placeholder="title"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        style={{ padding: 10, margin: 10 }}
+        type="text"
+        placeholder="description"
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <br />
+      <button
+        style={{ padding: 10, margin: 10 }}
+        onClick={() => {
+          fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then(async function (res) {
+              const json = await res.json();
+          alert(`Todo is completed: ${json.message || "Success"}`);                
+            })
+            .catch((err) => {
+              console.error("Error adding todo:", err);
+              alert("Failed to add todo");
+            });
+        }}
+      >
+        Add to todo
+      </button>
+    </div>
+  );
+}
