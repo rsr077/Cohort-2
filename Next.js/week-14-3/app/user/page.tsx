d2f09ea3f1610316eafb787a92eb6ebf5b85b28a
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-06-09 06:16:21 +0530

    new

---
+++ Next.js/week-14-3/app/user/page.tsx
@@ -0,0 +1,33 @@
+
+
+import axios from "axios";
+
+
+
+  async function fetchData()  {
+      const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+      console.log("response is"+ JSON.stringify(response.data));
+      return  response.data;
+  }
+
+export default async function User() {
+     const data = await fetchData();
+
+
+       return (
+     <div>
+      
+      {data.name}
+      {data.email}
+    </div>
+  );
+}
+  
+ 
+
+  
+     
+     
+  
+
+
