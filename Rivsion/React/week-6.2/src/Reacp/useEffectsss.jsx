800f3dd8000ba8cad3e1290b3197f4098ebc3093
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-06-18 18:49:07 +0530

    Init

--- Rivsion/React/week-6.2/src/Reacp/useEffectsss.jsx
+++ Rivsion/React/week-6.2/src/Reacp/useEffectsss.jsx
@@ -1,56 +1,49 @@
 import React, { useEffect, useState } from 'react';
 
-export const Solutin =() => {
- const [bankData, setBankData] = useState({});
-
- const [exchaggData, setExchaggData] = useState({});
- const [bankDatas, setBankDatas] = useState({});
-
-       
-  useEffect(() => {
-   
-      setTimeout(() => {
-        setBankData({
-          returns : 100
-        });
-      }, 1000);
-       }, []);
-
-
-       useEffect(() => {
-      setTimeout(() => {
-        setExchaggData({
-          income : 100
-        })
-      }, 3000);
-            }, [])
+export const Solutin = () => {
+  const [exchagg2Data, setExchagg2Data] = useState({});
+  const [exchaggData, setExchaggData] = useState({});
+  const [bankData, setBankData] = useState({});
 
   useEffect(() => {
-   
-      setTimeout(() => {
-        setBankDatas({
-          returns : 100
-        });
-      }, 1000);
-       }, []);
+    setTimeout(() => {
+      setBankData({
+        income: 100
+      });
+    }, 1000);
+  }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setExchaggData({
+        returns: 100
+      });
+    }, 3000);
+  }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setExchagg2Data({
+        returns: 100
+      });
+    }, 1000);
+  }, []);
 
-        const cryptreturns =  exchaggData.returns + bankData.returns ;
+  const cryptreturns = (exchaggData.returns || 0) + (exchagg2Data.returns || 0);
+  const ans = (cryptreturns + (bankData.income || 0)) * 0.3;
 
-       const ans = (cryptreturns + exchaggData.income) * 0.3
+  const isLoading =
+    exchaggData.returns === undefined ||
+    exchagg2Data.returns === undefined ||
+    bankData.income === undefined;
 
-  return  <div> 
-      
-
-
-
-
-
-
-       
-        <div>ans{ans}</div>
-
-       </div>
-  
-}
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>ans: {ans}</div>
+      )}
+    </div>
+  );
+};
