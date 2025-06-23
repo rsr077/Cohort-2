ea40b0592cfc180324947265ce86d7d848f91893
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-06-23 15:12:56 +0530

    week-19-1

---
+++ 100dayscode/week-19-offline-1/middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+let requestCount = 0;
+export function middleware(req: NextRequest, ) {
+
+  requestCount++;
+
+  const res  = NextResponse.next()
+ console.log("request count ", requestCount)
+  return res;
+}
